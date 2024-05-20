6b0ac8f5ad8ed8384ddeae9670d4cd42388216c3
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-20 16:33:55 +0530

    changes in 3rd variable script

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,7 +1,7 @@
 echo "Enter the value of Name1"
+read NAME1  #the value entered above will be automatically attached to NAME1
 echo "Enter the value of Name2"
-read NAME1
-read NAME2
+read NAME2  #the value entered above will be automatically attached to NAME2
 echo "$NAME1::how r u"
 echo "$NAME2::I am fine ,How r u"
 echo "$NAME1:i m good,why u r not coming to school"
