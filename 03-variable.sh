b73a37555a0717de856f774e76313af0b61b9463
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-20 16:29:36 +0530

    2nd variable script

---
+++ 03-variable.sh
@@ -0,0 +1,10 @@
+NAME1=echo "Enter the value of Name1"
+NAME2=echo "Enter the value of Name2"
+read NAME1
+read NAME2
+echo "$NAME1::how r u"
+echo "$NAME2::I am fine ,How r u"
+echo "$NAME1:i m good,why u r not coming to school"
+echo "$NAME2:I went to grand mon home for festival"
+echo "$NAME1:oh ! nice how is every one there"
+echo "$NAME2:well!every thing is fine there"
