6863db0d6264684b52c736890e87d502fe8d1e46
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-20 16:23:06 +0530

    variable script

---
+++ 01-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NAME1=Sravani
+NAME2=Mihir
+echo "$NAME1::how r u"
+echo "$NAME2::I am fine ,How r u"
+echo "$NAME1:i m good,why u r not coming to school"
+echo "$NAME2:I went to grand mon home for festival"
+echo "$NAME1:oh ! nice how is every one there"
+echo "$NAME2:well!every thing is fine there"
