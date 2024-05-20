54c38505e1a59da7430fe43d96ba80de992a0296
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-20 23:08:55 +0530

    replacing colors

--- 01-variable.sh
+++ 01-variable.sh
@@ -2,7 +2,7 @@
 
 echo -e  "\e[32m Ramu::how r u"
 echo -e "\e[31m geeta::I am fine ,How r u"
-echo -e "\e[33m Ramu:i m good,why u r not coming to school" \e[0m
+echo -e "\e[33m Ramu:i m good,why u r not coming to school \e[0m" 
 echo "geeta:I went to grand mon home for festival"
 echo "Ramu:oh ! nice how is every one there"
 echo "geeta:well!every thing is fine there"
