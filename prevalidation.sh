c9e19716147316b3cf20c027c01379edf7030472
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-20 23:52:23 +0530

    prevalidation

--- prevalidation.sh
+++ prevalidation.sh
@@ -2,12 +2,22 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOG_FILES=/tmp/$0_$TIMESTAMP.log
+VALIDATE(){
+    echo " $1 info is :" &>>$LOG_FILES
+}
 #fetching prevalidation outputs in log files
+VALIDATE() "Mmeory info"
 free -g &>>$LOG_FILES
+VALIDATE() "CPU info"
 lscpu &>>$LOG_FILES
+VALIDATE() "fstab info"
 cat /etc/fstab &>>$LOG_FILES
+VALIDATE() "filesystem info"
 df -h &>>$LOG_FILES
+VALIDATE() "ip address info"
 ifconfig &>>$LOG_FILES
+VALIDATE() "ip address config info"
+cat /etc/sysconfig/network-scripts/ifcf-eth* &>>$LOG_FILES
 cat $LOG_FILES
 echo "pre validation has been completed and log file stored in /tmp"
 if [ $ID -ne 0 ]
@@ -17,7 +27,7 @@ then
 else
    echo -e "\e[32m proceeding to reboot the server \e[0m"
 fi
-reboot &>>$LOG_FILES
+#reboot &>>$LOG_FILES
 
 
 
