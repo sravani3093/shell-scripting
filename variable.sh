7e405086d4aa4d7b3ecc01300fa2e8bb8b0c168f
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-20 16:18:53 +0530

    variable script

--- variable.sh
+++ variable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Ramu::how r u"
-echo "geeta::I am fine ,How r u
+echo "geeta::I am fine ,How r u"
 echo "Ramu:i m good,why u r not coming to school"
 echo "geeta:I went to grand mon home for festival"
 echo "Ramu:oh ! nice how is every one there"
