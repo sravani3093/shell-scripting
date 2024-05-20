75927f7c419c9dee8e109de2b492cbe4e0ed38c1
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-20 23:22:33 +0530

    prevalidation

--- prevalidation.sh
+++ prevalidation.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 ID=$(id -u)
-TIMESTAMP=$(date-+%F-%H:%M:%S)
+TIMESTAMP=$(date +%F-%H:%M:%S)
 LOG_FILES=/tmp_$0_$TIMESTAMP.log
 if [ $ID -ne 0 ]
 then
    echo -e "\e[31m ERROR:you are not root user,please login as root user and run \e[0m"
+   exit 1
 else
    echo -e "\e[32m starting the pre-validateion process \e[0m]"
 
