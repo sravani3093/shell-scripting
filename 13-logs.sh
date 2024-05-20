6de7c3ad4b92a5c2a883b4cebc5640dc8f4d733d
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-20 22:28:26 +0530

    logging the files

--- 13-logs.sh
+++ 13-logs.sh
@@ -2,23 +2,26 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%s)
 Log_file=/tmp/$0_$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo "ERROR: $2 is unsuccessfull"
+        echo -e " ERROR: $2 is  $R unsuccessfull $N"
         exit 1
     else 
-        echo " $2 is successfully"
+        echo -e " $2 is  $R successfully $N"
     fi
 }
 
 if [ $ID -ne 0 ]
 then
-   echo "ERROR:you are not root user,run with root user"
+   echo  -e "ERROR: $R you are not root user,run with root user $N"
    exit 1 #you can give other than 0
 else
-   echo "you are root user"
+   echo -e " $G you are root user $N"
 fi
 
 yum install mysql -y &>>$Log_file
