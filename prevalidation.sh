f46d81252aff8976e478461aaa42329f518ae936
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-20 23:24:48 +0530

    prevalidation

--- prevalidation.sh
+++ prevalidation.sh
@@ -7,14 +7,14 @@ then
    echo -e "\e[31m ERROR:you are not root user,please login as root user and run \e[0m"
    exit 1
 else
-   echo -e "\e[32m starting the pre-validateion process \e[0m]"
+   echo -e "\e[32m starting the pre-validateion process \e[0m"
 fi
 #fetching prevalidation outputs in log files
-free -g &>>$LOG_FILES
-lscpu &>>$LOG_FILES
-cat /etc/fstab &>>$LOG_FILES
-df -h &>>$LOG_FILES
-ifconfig &>>$LOG_FILES
+free -g 
+lscpu 
+cat /etc/fstab 
+df -h 
+ifconfig 
 
 
    
