b921cd0ab2fab1f21487a40ee9663e87959010f1
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-20 21:16:59 +0530

    DATE VARIABLE TYPE

--- 09-condition.sh
+++ 09-condition.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 Number= echo "enter the value:"
-read Number
+read Number #read the enter Number 
 if [ $Number -gt 100 ]
 then
      echo "the value $Number is greater than 100" # commands to execute if condition is true
 else
      echo "the value $Number is less than 100"  # commands to execute if condition is false
-fi
+fi #reverse of fi indicating that if condition  is completed
