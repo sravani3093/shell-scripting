bf0006a5f4588c36944bff96d5f536f790c0a554
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-20 23:41:37 +0530

    prevalidation

--- prevalidation.sh
+++ prevalidation.sh
@@ -2,13 +2,6 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOG_FILES=/tmp/$0_$TIMESTAMP.log
-if [ $ID -ne 0 ]
-then
-   echo -e "\e[31m ERROR:you are not root user,please run as root user \e[0m"
-   exit 1
-else
-   echo -e "\e[32m starting the pre-validateion process \e[0m"
-fi
 #fetching prevalidation outputs in log files
 free -g &>>$LOG_FILES
 lscpu &>>$LOG_FILES
