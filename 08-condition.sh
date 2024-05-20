1b201734d31ae0913e1e0b2234e386802638bc94
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-20 20:48:25 +0530

    DATE VARIABLE TYPE

--- 08-condition.sh
+++ 08-condition.sh
@@ -5,5 +5,5 @@ if [$Number -gt 100]
 then
      echo "the value $Number is greater than 100" # commands to execute if condition is true
 else
-    echo "the value $Number is less than 100"  # commands to execute if condition is false
+     echo "the value $Number is less than 100"  # commands to execute if condition is false
 fi
