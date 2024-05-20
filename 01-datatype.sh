f5261cc592cd81f02c993f7704efd2cdbd46be2d
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-20 17:00:32 +0530

    DATE VARIABLE TYPE

--- 01-datatype.sh
+++ 01-datatype.sh
@@ -1,6 +1,6 @@
 Fruits=(apple,banana,mango)
 
 echo "the first fruit is :${Fruits[0]}"
-echo "the  second fruit is :${Fruits[1]}"
-echo "the first fruit is :${Fruits[2]}"
-echo "the fruits are :${fruits[@]}"
+echo "the second fruit is :${Fruits[1]}"
+echo "the third fruit is :${Fruits[2]}"
+echo "the fruits are :${Fruits[@]}"
