5e0980007fefd3f3dd20d7f12b8b11b7a300d765
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-20 16:36:13 +0530

    user input should not echo

---
+++ .vs/shell-scripting/04-variable.sh
@@ -0,0 +1,10 @@
+echo "Enter the value of Name1"
+read  -s NAME1  #the value entered above will be automatically attached to NAME1
+echo "Enter the value of Name2"
+read  -s NAME2  #the value entered above will be automatically attached to NAME2
+echo "$NAME1::how r u"
+echo "$NAME2::I am fine ,How r u"
+echo "$NAME1:i m good,why u r not coming to school"
+echo "$NAME2:I went to grand mon home for festival"
+echo "$NAME1:oh ! nice how is every one there"
+echo "$NAME2:well!every thing is fine there"
