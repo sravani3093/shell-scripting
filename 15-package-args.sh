cbf322122ce9ff6bd19f0950608a6c2ec39c52ed
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-21 14:06:24 +0530

    package installationaaaargs

--- 15-package-args.sh
+++ 15-package-args.sh
@@ -6,35 +6,37 @@ Y="\e[33m"
 N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE=/tmp/$0_$TIMESTAMP.log
+echo "script started executing at : $TIMESTAMP " &>>$LOGFILE
 
-VALIDATE() {
+VALIDATE() { #function created to validate installation of packages
     if [ $1 -ne 0 ]
     then 
        echo -e "$2 IS INSTALLATION IS .......$R FAILED $N "
     else
        echo -e " $2 IS INSTALLED........ $G SUCCESSFULLY $N "
-    fi
+    fi #reverse of if end of if loop
 }
 
-echo "script started executing at : $TIMESTAMP" &>>$LOGFILE
-
-if [ $ID -ne 0 ]
+if [ $ID -ne 0 ] #if id 0 then it is root user 
 then 
     echo -e " $R please run the script with root user $N "
     exit 1
 else
     echo -e " $G proceeding to run the package installation $N "
-fi
+fi #reverse of if end of if loop
+# mysql postfix are package
+#first package will be taken as argument 
+#$@ represent all arguments echoing
 for PACKAGE in $@
 do
-    yum  list install $PACKAGE &>>$LOGFILE
+    yum  list installed $PACKAGE &>>$LOGFILE  #checking the package installation file in server
     if [ $? -ne 0 ]
     then 
-        yum install $PACKAGE &>>$LOGFILE
+        yum install $PACKAGE  -y &>>$LOGFILE #installing hte package
         VALIDATE $? "INSTALLATION OF $PACKAGE"
     else
-        echo " $PACKAGE IS ALREADY INSTALLED.... $Y SKIPPING $N "
+        echo " $PACKAGE IS ALREADY INSTALLED.... $Y SKIPPING $N " #if package is already installed skipping 
     fi
-done
+done #end of for loop
  
- echo " The number of arguments passed in $0 are : $# "
+ echo " The number of arguments passed in $0 are : $# " #how many arguments passed in the script
