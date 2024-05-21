2586a520345200c36ad8c07a04a02d9f6ca3962f
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-21 13:47:09 +0530

    package installationaaaargs

--- 15-package-args.sh
+++ 15-package-args.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 ID=$(id -u) #to check root user login
-R=\e[31m
-G=\e[32m
-Y=\e[33m
-N=\e[0m
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE=/tmp/$0_$TIMESTAMP.log
 
 VALIDATE() {
     if [ $1 -ne 0 ]
     then 
-       echo -e "$2 IS INSTALLATION IS .......$R FAILED $N"
+       echo -e "$2 IS INSTALLATION IS .......$R FAILED $N "
     else
-       echo -e "$2 IS INSTALLED........ $G SUCCESSFULLY $N"
+       echo -e " $2 IS INSTALLED........ $G SUCCESSFULLY $N "
     fi
 }
 
@@ -20,19 +20,19 @@ echo "script started executing at : $TIMESTAMP" &>>$LOGFILE
 
 if [ $ID -ne 0 ]
 then 
-    echo -e " $R please run the script with root user $N"
+    echo -e " $R please run the script with root user $N "
     exit 1
 else
-    echo -e " $G proceeding to run the package installation $N"
+    echo -e " $G proceeding to run the package installation $N "
 fi
 for PACKAGE in $@
 do
     yum  list install $PACKAGE &>>$LOGFILE
     if [ $? -ne 0 ]
     then 
-        echo "$PACKAGE IS ALREADY INSTALLED.... $Y SKIPPING $N"
+        echo " $PACKAGE IS ALREADY INSTALLED.... $Y SKIPPING $N "
     else
         yum install $PACKAGE &>>$LOGFILE
-        VALIDATE $? " INSTALLATION OF $PACKAGE"
+        VALIDATE $? "INSTALLATION OF $PACKAGE"
     fi
 done
