8c461bfebe1828cd90466cf8cd8d2f87ea676e0d
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-29 10:40:03 +0530

    version

--- kernelversion.sh
+++ kernelversion.sh
@@ -4,11 +4,12 @@ VERSION=$(uname -a)
 For i in hosts
 {
     $VERSION | grep -i *.212
-    if [$? -ne 0 ];then
-    {
+    if [ $? -ne 0 ]
+    then
+    
         echo " There are no default kernel value"
     else
         echo "kernel varsion of host are :$i"
-    }
+    
     fi
 }
