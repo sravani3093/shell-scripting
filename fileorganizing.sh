36557034dd68a3d3cef7a023544bb3dbe62212f7
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-29 13:57:02 +0530

    version

--- fileorganizing.sh
+++ fileorganizing.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-TARGETDIR="/tmp/sravani"
+
 SOURCE="/home/centos"
 TIMESTAMP=$(date +%F-%H-%M-%s)
 DATE=$(date)
+cd /tmp/sravani
+mkdir  -p /Script_$TIMESTAMP
 
 cd $SOURCE
 PATH=$(ls -lrt |grep "$(date +'%b %d')" |awk '{print $9}')
 while IFS= read -r line
 do
+   cp $line  /tmp/sravani/Script_$TIMESTAMP
    echo "file has  to been moved is $line"
-    
-    
+
 done <<< $PATH
-   
-cd $TARGETDIR
-mkdir  -p /Script_$TIMESTAMP
-cp $line  /Script_$TIMESTAMP
