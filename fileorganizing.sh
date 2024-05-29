566e960024ba08c139ddb92b84e9bd2b1e10675c
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-29 13:58:43 +0530

    version

--- fileorganizing.sh
+++ fileorganizing.sh
@@ -10,7 +10,5 @@ cd $SOURCE
 PATH=$(ls -lrt |grep "$(date +'%b %d')" |awk '{print $9}')
 while IFS= read -r line
 do
-   cp $line  /tmp/sravani/Script_$TIMESTAMP
-   echo "file has  to been moved is $line"
-
+   rm-rf $line 
 done <<< $PATH
