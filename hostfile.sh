0390c2a7ecb5fa4b8105a2244bd95e4fbbc08ba6
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-29 11:03:06 +0530

    version

--- hostfile.sh
+++ hostfile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 USERNAME=centos
-HOSTS="/home/centos/hosts"
+HOSTS="ip-172-31-22-212.ec2.internal ip-172-31-19-231.ec2.internal ip-172-31-16-84.ec2.internal "
 SCRIPT="/home/centos/shell-scripting/sh kernelversion.sh"
 for HOSTNAME in ${HOSTS} ; do
     ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
