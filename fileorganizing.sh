5779e887693ae95ef1b5e2cc434ff9d631e192fa
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-29 14:53:30 +0530

    version

--- fileorganizing.sh
+++ fileorganizing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TARGET_DIR="/tmp/sravani"
 SOURCE_DIR="/home/centos/sravani"
-PATH=$(ls -lrt $SOURCE_DIR |grep "$(date +'%b %d')" |awk '{print $9}')
+PATH=$(ls -lrt /home/centos/sravani |grep "$(date +'%b %d')" |awk '{print $9}')
 
 echo "$PATH"
