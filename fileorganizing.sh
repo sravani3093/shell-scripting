877759cceacd105f1fb613f1d2f8485cda75bbf0
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-29 13:24:38 +0530

    version

--- fileorganizing.sh
+++ fileorganizing.sh
@@ -5,16 +5,17 @@ read $SOURCE
 TIMESTAMP=$(date +%F-%H-%M-%s)
 DATE=$(date)
 
-if [ -f $SOURCE]
+if [ -f $SOURCE ]
 then
    cd $SOURCE
    PATH=$(ls -lrt |grep "$(date +'%b %d')" |awk '{print $9}')
-   if [ $? ne 0]
+   if [ $? ne 0 ]
    then
        echo "there are no files generated on :$DATE"
     else
-       FILE=$(mkdir -P /$TARGETDIR/scripts_$TIMESTAMP)
-       mv $PATH $FILE
+       cd $TARGETDIR
+       DIR=$(mkdir Script_$TIMESTAMP)
+       mv $PATH $DIR
     fi
 else 
     echo "Directory not exist"
