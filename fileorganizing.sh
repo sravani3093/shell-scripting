9a79e5b2660fd5d622c1b278e7a59fd967015df5
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-29 13:49:34 +0530

    version

--- fileorganizing.sh
+++ fileorganizing.sh
@@ -4,21 +4,15 @@ SOURCE="/home/centos"
 TIMESTAMP=$(date +%F-%H-%M-%s)
 DATE=$(date)
 
-if [ -d $SOURCE ]
-then
-   cd $SOURCE
-   PATH=$(ls -lrt |grep "$(date +'%b %d')" |awk '{print $9}')
-   while IFS= read -r line
-   do
-      #cd $TARGETDIR
-      #mkdir Script_$TIMESTAMP
-      #cp $line  /Script_$TIMESTAMP
-      #echo "file has been moved successfully: $line"
-      echo "Deleting file: $line"
-      rm -rf $line
+cd $SOURCE
+PATH=$(ls -lrt |grep "$(date +'%b %d')" |awk '{print $9}')
+while IFS= read -r line
+do
+   cd $TARGETDIR
+   mkdir Script_$TIMESTAMP
+   cp $line  /Script_$TIMESTAMP
+   echo "file has been moved successfully: $line"
     
-    done <<< $PATH
-else 
-    echo "Directory not exist"
-fi
-       
+    
+done <<< $PATH
+   
