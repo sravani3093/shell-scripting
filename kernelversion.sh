4a780e8cf4a37af2799954f3373d88b1bfd3d1ae
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-29 11:08:51 +0530

    version

--- kernelversion.sh
+++ kernelversion.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
+FILE=/home/centos/shell-scripting/hosts
 VERSION=$(uname -r)
-for i in hosts
+for i in ${$FILE}
 {
-    
+    ssh centos@i
     $VERSION | grep -i 4.18.0-535.*
     if [ $? -ne 0 ]
     then
     
         echo " There are no default kernel value"
     else
-        echo "kernel varsion of host are :$i"
+        echo "kernel varsion of host are :$"
     
     fi
     exit
