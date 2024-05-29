a67839cab64f9292db593a1a4e86b9570e0acd3d
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-29 13:21:05 +0530

    version

--- fileorganizing.sh
+++ fileorganizing.sh
@@ -7,7 +7,7 @@ DATE=$(date)
 
 if [ -f $SOURCE]
 then
-   PATH=$(find $SOURCE  -type f -mtime date -name  "*")
+   PATH=$(ls -lrt |grep "$(date +'%b %d')" |awk '{print $9}')
    if [ $? ne 0]
    then
        echo "there are no files generated on :$DATE"
