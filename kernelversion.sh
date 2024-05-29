d8cbc598f577f5a71c895d49d653ad0cc615fdc8
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-29 11:20:36 +0530

    version

--- kernelversion.sh
+++ kernelversion.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
-#HOSTS="ip-172-31-22-212.ec2.internal ip-172-31-19-231.ec2.internal ip-172-31-16-84.ec2.internal "
-FILE="/home/centos/shell-scripting/hosts"
+HOSTS="ip-172-31-22-212.ec2.internal ip-172-31-19-231.ec2.internal ip-172-31-16-84.ec2.internal "
+#FILE="/home/centos/shell-scripting/hosts"
 VERSION=$(uname -r)
-#USERNAME=centos
-#for HOSTNAME in ${HOSTS} ; do
-  ##  ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
-#done
-for hosts in ${FILE[@]}
-do
-
-    ssh centos@$hosts
-    $VERSION | grep -i 4.18.0-535.*
+USERNAME=centos
+for HOSTNAME in ${HOSTS} ; do
+   ssh -l ${USERNAME} ${HOSTNAME} 
+   $VERSION | grep -i 4.18.0-535.*
     if [ $? -ne 0 ]
     then
     
         echo " There are no default kernel value"
     else
-        echo "kernel varsion of host are :$hosts"
+        echo "kernel varsion of host are :$HOSTNAME"
     
     fi
-    exit
 
 done
+
+
+    
