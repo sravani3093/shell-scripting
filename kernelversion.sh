d3722ad11fe597e5100c2a77a15e50bb78f89a74
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-29 11:18:26 +0530

    version

--- kernelversion.sh
+++ kernelversion.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-FILE="/home/centos/shell-scripting/hosts"
+#HOSTS="ip-172-31-22-212.ec2.internal ip-172-31-19-231.ec2.internal ip-172-31-16-84.ec2.internal "
+FILE="//home/centos/shell-scripting/hosts"
 VERSION=$(uname -r)
+#USERNAME=centos
+#for HOSTNAME in ${HOSTS} ; do
+  ##  ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
+#done
 for hosts in ${FILE[@]}
-{
-    ssh centos@hosts
+do
+
+    ssh centos@$hosts
     $VERSION | grep -i 4.18.0-535.*
     if [ $? -ne 0 ]
     then
@@ -14,4 +20,5 @@ for hosts in ${FILE[@]}
     
     fi
     exit
-}
+
+done
