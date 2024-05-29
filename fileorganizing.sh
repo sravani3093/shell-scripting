2ea4d9b02e34c1f031458eeb17da3b48f80d3442
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-29 14:30:26 +0530

    version

--- fileorganizing.sh
+++ fileorganizing.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 TARGET_DIR="/tmp/sravani"
 SOURCE_DIR="/home/centos/sravani"
-for file in "$SOURCE_DIR"
+PATH=$(ls -lrt $SOURCE_DIR |grep "$(date +'%b %d')" |awk '{print $9}')
+for file in "$PATH"
 do
    if [[ -f $file ]];
    then
-   PATH=$(ls -lrt $SOURCE_DIR |grep "$(date +'%b %d')" |awk '{print $9}')
+   
    echo "$PATH"
    fi
 done
